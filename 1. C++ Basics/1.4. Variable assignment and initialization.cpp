c009dc56a7cdc7daae5c8207c24bcea407a29d15
Dennis Jonathan <dennis.jonathan78@gmail.com>
2023-05-31 21:44:56 +0700

    Removed [[maybe_unused]] from 1.4.

--- 1. C++ Basics/1.4. Variable assignment and initialization.cpp
+++ 1. C++ Basics/1.4. Variable assignment and initialization.cpp
@@ -3,10 +3,10 @@
 int main()
 {  
    // Variable instantiation
-   [[maybe_unused]] int x; // Define x as an integer variable
-   [[maybe_unused]] double width; // Define width as a double variable
+   int x; // Define x as an integer variable
+   double width; // Define width as a double variable
 
-   [[maybe_unused]] int a, b; // Define multiple variables of the same type
+   int a, b; // Define multiple variables of the same type
 
 
    // Variable assignment
@@ -28,20 +28,20 @@ int main()
    */
    [[maybe_unused]] int xx { 5 }; // Initialize x to 5 (direct list initialization)
    [[maybe_unused]] int xxx = { 5 }; // Initialize x to 5 (copy list initialization)
-   [[maybe_unused]] int xxxx {}; // Initialize x to 0 (value initialization)
+   int xxxx {}; // Initialize x to 0 (value initialization)
 
    std::cout << xxxx << std::endl; // prints 0
 
    // Initialization of multiple variables
-   [[maybe_unused]] int xa { 5 }, ya { 6 }; // Initialize x to 5 and y to 6
-   [[maybe_unused]] int xb = 5, yb = 6; // Initialize x to 5 and y to 6 (copy initialization)
-   [[maybe_unused]] int xc(5), yc(6); // Initialize x to 5 and y to 6 (direct initialization)
-   [[maybe_unused]] int i {}, j {}; // Initialize i and j to 0 (value initialization)
+   int xa { 5 }, ya { 6 }; // Initialize x to 5 and y to 6
+   int xb = 5, yb = 6; // Initialize x to 5 and y to 6 (copy initialization)
+   int xc(5), yc(6); // Initialize x to 5 and y to 6 (direct initialization)
+   int i {}, j {}; // Initialize i and j to 0 (value initialization)
 
    std::cout << xa << std::endl; // prints 5
 
    // Maybe Unused Variables
-   [[maybe_unused]] double pi {3.14}; // The compiler will not warn about this unused variable, -Werror will bite you in the ass if not
+   double pi {3.14}; // The compiler will not warn about this unused variable, -Werror will bite you in the ass if not
 
    return 0;
 }
